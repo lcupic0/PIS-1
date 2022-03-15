660612634fa41e42b62ef53e2ba2d40275edff40
lcupic0 <lcupic123@gmail.com>
2022-03-15 16:55:39 +0100

    nova linija

--- vjezba1.c
+++ vjezba1.c
@@ -3,8 +3,10 @@
 
 int main() {
 
-	printf("Prva vjezba iz predmeta Projektiranje informacijskih sustava!");
+	printf("Prva vjezba iz predmeta Projektiranje informacijskih sustava.");
 	printf("\n");
+
+	printf("Nova izmjena!");
 
 	return 0;
 }
